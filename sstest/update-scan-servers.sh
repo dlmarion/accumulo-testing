37150ead022d52533f42c822bdefc6af2951e41a
Keith Turner <kturner@apache.org>
2022-05-24 20:29:14 +0000

    enable scan server metrics

--- sstest/update-scan-servers.sh
+++ sstest/update-scan-servers.sh
@@ -6,6 +6,7 @@ ACCUMULO_DOCKER_DIR=~/git/accumulo-docker
 cd $ACCUMULO_SRC_DIR
 mvn clean package -DskipTests
 cp assemble/target/accumulo-2.1.0-SNAPSHOT-bin.tar.gz $ACCUMULO_DOCKER_DIR
+cp test/target/accumulo-test-2.1.0-SNAPSHOT.jar $ACCUMULO_DOCKER_DIR
 
 cd $ACCUMULO_DOCKER_DIR
 docker build --build-arg ACCUMULO_VERSION=2.1.0-SNAPSHOT --build-arg ACCUMULO_FILE=accumulo-2.1.0-SNAPSHOT-bin.tar.gz   --build-arg HADOOP_VERSION=3.3.2 --build-arg HADOOP_FILE=hadoop-3.3.2.tar.gz   --build-arg ZOOKEEPER_VERSION=3.5.9 --build-arg ZOOKEEPER_FILE=apache-zookeeper-3.5.9-bin.tar.gz -t accumulo .
